9f91df052b6f1befc98a24777f2f2c38dc3dfdab
unknown <jaylan210@gmail.com>
2020-07-14 22:49:16 -0400

    slight change to menu page

--- src/views/Menus.vue
+++ src/views/Menus.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="paper-bg">
-      <h1>The Cowfish menu is huge.</h1>
-      <p>
-        Huge as an elephant. Huger than a cruise ship. More huger than the Empire State Building! Well, maybe not that huge. But there’s so much to choose from that you can even find something cranky Aunt Mildred will eat.
-        <br/>
-        AND, we’re picky about our ingredients. Only the freshest and purest welcome here. 
-      </p>
+      <div class="menu-content">
+        <h1>The Cowfish menu is huge.</h1>
+        <p>
+            Huge as an elephant. Huger than a cruise ship. More huger than the Empire State Building! Well, maybe not that huge. But there’s so much to choose from that you can even find something cranky Aunt Mildred will eat.
+            <br/>
+            AND, we’re picky about our ingredients. Only the freshest and purest welcome here. 
+        </p>
+      </div>
 
       <div class="menu-buttons">
           <a href="https://thecowfish.com/The_Cowfish_menu_Charlotte.pdf" target="_blank" rel="noopener noreferrer">
@@ -28,11 +30,14 @@
                 </div>
                 <div>
                     <h2>Burgers + Chicken</h2>
-                    <p>
-                        100% all-natural (not fake) chicken, Angus ground beef, turkey, lamb, or bison. Raised humanely--never spanked, only put in time-out.
-                        <br/>
-                        Never, ever given antibiotics or hormones.
-                    </p>
+                    <ul>
+                        <li>
+                            100% all-natural <b>(not fake)</b> chicken, Angus ground beef, turkey, lamb, or bison. Raised humanely--never spanked, only put in time-out.
+                        </li>
+                        <li>
+                            Never, ever given antibiotics or hormones.
+                        </li>
+                    </ul>
                 </div>
             </div>
 
@@ -42,11 +47,14 @@
                 </div>
                 <div>
                     <h2>Sushi & Fish</h2>
-                    <p>
-                        Sustainable, antibiotic-free and non-GMO. ‘Cause that’s just how we roll.
-                        <br/>
-                        Bigeye tuna is rod & reel caught in the US and Canada. Reely. No foolin’.
-                    </p>
+                    <ul>
+                        <li>
+                            Sustainable, antibiotic-free and non-GMO. ‘Cause that’s just how we roll.
+                        </li>
+                        <li>
+                            Bigeye tuna is rod & reel caught in the US and Canada. Reely. No foolin’.
+                        </li>
+                    </ul>
                 </div>
             </div>
 
@@ -56,11 +64,14 @@
                 </div>
                 <div>
                     <h2>Shellfish</h2>
-                    <p>
-                        Chemical-free—passes the drug test every time.
-                        <br/>
-                        From the good ol’ U.S. of A. 
-                    </p>
+                    <ul>
+                        <li>
+                            Chemical-free—passes the drug test every time.
+                        </li>
+                        <li>
+                            From the good ol’ U.S. of A. 
+                        </li>
+                    </ul>
                 </div>
             </div>
 
@@ -70,11 +81,14 @@
                 </div>
                 <div>
                     <h2>Salads & Produce</h2>
-                    <p>
-                        Locally sourced whenever possible, ‘cause we know how to pick ‘em.
-                        <br/>
-                        WARNING: Salads may exceed the size of your head. 
-                    </p>
+                    <ul>
+                        <li>
+                            Locally sourced whenever possible, ‘cause we know how to pick ‘em.
+                        </li>
+                        <li>
+                            WARNING: Salads may exceed the size of your head.
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -97,6 +111,9 @@ export default {
 </script>
 
 <style scoped>
+.menu-content {
+    padding: 15px;
+}
 
 .menu-buttons .btn {
     margin: 10px;
@@ -104,10 +121,10 @@ export default {
 
 .food-item {
     background: transparent;
+    box-shadow: 0px 3px 15px rgba(0,0,0,0.2);
+
     margin: 10px;
     padding: 10px;
-
-    box-shadow: 0px 3px 15px rgba(0,0,0,0.2);
 }
 
 .food-information div img {
@@ -116,6 +133,11 @@ export default {
 }
 
 @media(min-width: 768px) {
+    .menu-content {
+        width: 60%;
+        margin: auto;
+    }
+
     .food-information {
         display: grid;
         grid-template-columns: repeat(4, 1fr);
