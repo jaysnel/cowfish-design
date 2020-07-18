e22b5c85df116fc40e6e90a5b39095fb4d7ba6f1
Jaylan Snelson <jaylan210@gmail.com>
2020-07-18 10:57:01 -0400

    hopefully this fixes the issue

--- src/App.vue
+++ src/App.vue
@@ -56,6 +56,7 @@ export default {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   background: #000;
+  max-width: 100%;
 }
 
 .mobile-nav-section-with-logo > div img {
@@ -73,6 +74,7 @@ export default {
 
 #nav,
 #mobile-nav {
+  max-width: 100%;
   padding: 30px;
   background: #000;
 }
